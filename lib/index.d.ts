3d3e6cc73fe251a17df65a499b0138679f12575b
HembitskiAndrei <shaftdg3@gmail.com>
2022-04-20 13:32:12 +0300

    feat: init commit

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import { Scene } from '@babylonjs/core';
+import * as ZapparBabylon from '@zappar/zappar-babylonjs-es6';
+export declare const scene: Scene;
+export declare const camera: ZapparBabylon.Camera;
